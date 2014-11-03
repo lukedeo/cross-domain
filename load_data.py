d19a89db3361b2440191ddd780049888e94b6fb4
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-11-03 10:17:00 -0800

    making pipleine

--- load_data.py
+++ load_data.py
@@ -9,6 +9,7 @@ from gensim import corpora, models, similarities
 json_data = open('data/amazon_products').read()
 
 # They didn't format the JSON properly -- this fixes that
+# this 
 json_data = json_data.replace('\x01', '').replace('\n', '')
 
 # build a list of tuples where each 
@@ -21,54 +22,39 @@ for pos in re.finditer('}{', json_data):
 
 
 # how many items do we want to load? 10 to test.
-n = 500
+
 
 # load each review as a python dict, store them in a list
-amazon = [json.loads(json_data[idx[0] : idx[1]]) for idx in ix[:n]]
-
-####### Load the hierarchy
-heirarchy = open('data/AmazonHeirarchy.json').read()
-
-labels = json.loads(heirarchy)
-
-root_labels = {}
-
-for lab in labels:
-    root_labels[int(lab['BrowseNodeId'])] = lab['Name']
-
-#######
-
-def get_top_label(item, root_labels):
-    return [root_labels[it] for it in get_parents(item) if it in root_labels.keys()]
 
 
 # simple example of what we can do -- we can pull 
 # review content and tie it the ASIN -- the unique product ID
 # We can also write out the product categories, etc.
 
-def grab_reviews(item, reviewtype = 'PrunedReviews'):
-    return [{'ASIN': item["ASIN"], 'text' : it['Content'], 'labels' : get_labels(item, graphs)} for it in item[reviewtype]]
+def grab_reviews(item, graphs, reviewtype = 'PrunedReivews'):
+    return [{'ASIN': item["ASIN"], 
+             'text' : it['Content'], 
+             'labels' : get_labels(item, graphs)} for it in item[reviewtype]]
 
-reviews = [review for item in (grab_reviews(t['Item']) for t in amazon) for review in item]
 
-corpus = [review['text'] for review in reviews]
+
+n = 100
+
+amazon = (json.loads(json_data[idx[0] : idx[1]]) for idx in ix[:n])
+
+reviews = (review for item in (grab_reviews(t['Item'], graphs) for t in amazon) for review in item)
+
+corpus = (review['text'] for review in reviews)
 
 stoplist = stopwords.words('english')
 
-texts = [[word for word in document.lower().split() if word not in stoplist] for document in corpus]
+texts = ((word for word in document.lower().split() if word not in stoplist) for document in corpus)
 
-texts = [[re.sub("\.|,|\"|-|\'|`|\*","", word) for word in text] for text in texts]
+texts = ((re.sub("\.|,|\"|-|\'|`|\*","", word) for word in text) for text in texts)
 
-texts = [[word for word in text if (word.isalpha() & (len(word) > 1))] for text in texts]
+texts = ((word for word in text if (word.isalpha() & (len(word) > 1))) for text in texts)
 
-all_tokens = sum(texts, [])
-
-tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-
-texts = [[word for word in text if word not in tokens_once] for text in texts]
-
-reformatted = [' '.join(words) for words in texts]
-
+reformatted = (' '.join(words) for words in texts)
 
 
 

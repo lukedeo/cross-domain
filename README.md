27348b30a79920f1e6e1dd259d778097e9add3d9
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-11-08 12:34:43 -0800

    readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ cross-domain
 ============
 
 Cross domain classification for ML class
+
+You'll have to add `/path/to/cross-domain/python/crossdomain` to your `PYTHONPATH`
+
+Look at `create-partitions.py` to see how you can partition the amazon data, then save it.
+
+Working on a script to load the individual pieces into  a nice format.
--- create-partitions.py
+++ create-partitions.py
@@ -5,11 +5,14 @@ import crossdomain as domain
 if __name__ == '__main__':
     
     print 'Loading Data...'
-    json_data = open('project/data/amazon_products_cleaned').read()
+    json_data = open('data/amazon_products_cleaned').read()
 
     print 'Parsing hierarchy...'
-    graphs = domain.get_amazon_graphs('project/data/AmazonHeirarchy.json')
+    graphs = domain.get_amazon_graphs('data/AmazonHeirarchy.json')
 
     n_partitions = 1000
 
-    domain.product_partition(json_data, graphs, n_partitions, prefix = 'project/amazon-data')
+    domain.product_partition(json_data, graphs, n_partitions, prefix = 'amazon-data')
+
+
+    
